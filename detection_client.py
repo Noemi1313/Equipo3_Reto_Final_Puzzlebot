9a2448fc5ce14aa5e6a55a97768a8f662af6a4f1
Noemi1313 <51099544+Noemi1313@users.noreply.github.com>
2022-06-18 18:36:21 -0500

    Update detection_client.py

--- detection_client.py
+++ detection_client.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+# Codigo utilizado para reconocimeinto de señales (Cliente)
+# Equipo 3
+# Noemi Carolina Guerra Montiel A00826944
+# Maria Fernanda Hernandez Montes A01704918
+# Mizael Beltran Romero A01114973
+# Izac Saul Salazar Flores A01197392
+# Junio 2022
+
+# Librerias
 import socket, time
 import cv2, json
 import numpy as np
@@ -8,14 +17,17 @@ from threading import Thread
 
 TCP_IP = 'localhost'
 
+# Socket
 sock = socket.socket()
 sock.connect((TCP_IP, TCP_PORT))
 
+# Acceder a webcam
 cap = cv2.VideoCapture(0)
 
 c = 0
 avg = 0
 
+# Cambiar el tamaño de la imagen
 def resize_img(frame):
   h = frame.shape[0]
   frame = frame[h//3:]
@@ -25,19 +37,23 @@ def resize_img(frame):
   return frame
 
 
+# Mientras la webam esta abierta
 while cap.isOpened():
   start = time.time()
+  # Leer la imagen
   ret, frame = cap.read()
   if(not ret or frame is None):
     break
 
   # frame = resize_img(frame)
   encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+  # Obtener el resultado
   result, imgencode = cv2.imencode('.jpg', frame, encode_param)
   data = np.array(imgencode)
   data = data.tobytes()
   send_variable_length(sock, data)
   detection = recv_variable_length(sock).decode()
+  # Labels
   detection = json.loads(detection)
   end = time.time()
   avg = (avg*c + (end - start))/(c + 1)
