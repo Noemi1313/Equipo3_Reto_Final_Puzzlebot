f5ef5e14636fef7788968ec49cda321458160d7e
Noemi1313 <51099544+Noemi1313@users.noreply.github.com>
2022-06-18 18:38:29 -0500

    Update detection_server.py

--- detection_server.py
+++ detection_server.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+# Codigo utilizado para reconocimeinto de señales (Server)
+# Equipo 3
+# Noemi Carolina Guerra Montiel A00826944
+# Maria Fernanda Hernandez Montes A01704918
+# Mizael Beltran Romero A01114973
+# Izac Saul Salazar Flores A01197392
+# Junio 2022
+
+# Librerias
 import socket
 import cv2, json
 import numpy as np
@@ -6,9 +15,11 @@ import darknet as dn
 from socketing import recv_variable_length, send_variable_length
 from socketing import TCP_PORT
 
+# Cargar los pesos y configuraciones de los datos
 net = dn.load_net(b"cfg/ois.cfg", b"weights/ois_final.weights", 0)
 meta = dn.load_meta(b"cfg/ois.data")
 
+# Socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', TCP_PORT))
 s.listen(True)
@@ -17,6 +28,7 @@ while True:
     conn, addr = s.accept()
     try:
         while True:
+            # Mandar imagen y realizar deteccion
             bytedata = recv_variable_length(conn)
             frame = np.frombuffer(bytedata, dtype='uint8')
             frame = cv2.imdecode(frame, 1)
